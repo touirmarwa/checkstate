07582563aeac2e45be0ffe3bde2cfa9be26c2a74
touirmarwa <touirmarwa1997@gmail.com>
2022-06-09 13:24:14 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      tab:[{title:'Name',id:Math.random(),IsDone:false},
+           {title:'Age',id:Math.random(),IsDone:false},
+           {title:'Adresse',id:Math.random(),IsDone:false},
+           {title:'Email',id:Math.random(),IsDone:false}
+          ],
+          textT:" "
 
-export default App;
+          }
+  }
+  handleAdd=()=> this.setState({tab : [...this.state.tab,{title:this.state.textT,id:Math.random(),IsDone:true}]})
+  handleDelete=(a)=> this.setState({tab : this.state.tab.filter((el)=> el.id !== a)}) 
+  handleEdit=(a)=> this.setState({tab : this.state.tab.map(el=> el.id === a ?{...el,IsDone : !el.IsDone} : el)})
+  render(){
+    return(
+      <div>
+        <h1>Challenge State</h1>
+        {
+          this.state.tab.map((el,i)=> 
+          <div key={el.id}>
+            <h2 className={el.IsDone && 'done'}>{el.title}</h2>
+            <h2>{el.IsDone ? 'True' : 'False'}</h2>
+            <button onClick={()=>this.handleDelete(el.id)}>Delete</button>
+            <button onClick={()=>this.handleEdit(el.id)}>Done</button>
+          </div>)
+        }
+        <br/>
+        <div>
+          <input type='text' onChange={(e)=> this.setState({textT : e.target.value})}/>
+          <button onClick={this.handleAdd}>Add</button>
+        </div>
+        
+      </div>
+    )
+  }
+}
+export default App
+
+
